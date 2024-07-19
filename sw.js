86c5e55bbc777471dd4097e0ba319890904844da
RomanKashnikov <kashnikov05@list.ru>
2024-07-19 10:57:27 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const staticCacheName = 's-app-v1'
+
+const assetUrls = [
+    'index.html',
+    'style_app.css',
+    'images',
+    'ios'
+]
+
+
+self.addEventListener('install', async event => {
+    const cache = await caches.open(steticCacheName)
+    await cache.addAll(assetUrls)
+})
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch', event.request)
+
+    event.respondWith(cacheFirst(event.request))
+})
+
+async function cacheFirst(request) {
+    const cached = await caches.match(request)
+    return cached ?? await fetch(request)
+}
